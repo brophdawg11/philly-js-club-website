795af053c273b02ac67ebf11ee4d211d3494ef68
Matt Brophy <matt@brophy.org>
2024-05-10 17:28:41 -0400

    Add prerendering

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,19 @@ export default defineConfig({
 	plugins: [
 		remix({
 			ignoredRouteFiles: ["**/.*"],
+			prerender: [
+				"/",
+				"/about",
+				"/ad",
+				"/code-of-conduct",
+				"/events",
+				// Found a bug in our pre-rendering!  We don't handle pre-rendering
+				// resource routes right now - so we'll need to keep doing that via
+				// wget until I can get that fixed in RR
+				// "ics-feed.ics",
+				"/join-us",
+				"/sponsors",
+			],
 		}),
 		tsconfigPaths(),
 	],
